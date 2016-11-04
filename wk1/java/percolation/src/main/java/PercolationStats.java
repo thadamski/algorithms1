cda7c583733402b81e583170b45785c92ee863c0
Tommy Adamski <tadamski@rzsoftware.com>
2016-11-03 21:43:34 -0400

    Finished PercolationStatus but values high

--- wk1/java/percolation/src/main/java/PercolationStats.java
+++ wk1/java/percolation/src/main/java/PercolationStats.java
@@ -1,38 +1,111 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 /**
  * Created by tadamski on 11/1/16.
  */
 public class PercolationStats {
+
+    private final int n;
+    private final int trials;
+    private final double[] percolationThresholds;
+    private final double mean;
+    private final double stddev;
 
     /** perform trials independent experiments on an n-by-n grid */
     public PercolationStats(int n, int trials) {
         if (n <= 0) throw new IllegalArgumentException("n <= 0");
         if (trials <= 0) throw new IllegalArgumentException("trials <= 0");
 
-        throw new UnsupportedOperationException();
+        this.trials = trials;
+        this.n = n;
+        this.percolationThresholds = new double[trials];
+
+        int gridSize = n * n;
+
+        for (int i = 0; i < trials; i++) {
+            // state variable to track how many sites are open
+            double opened = 0d;
+            Percolation p = new Percolation(n);
+
+            while (!p.percolates()) {
+                Site<Integer, Integer> site = findRandomClosedSite(p);
+                p.open(site.row, site.col);
+                opened++;
+            }
+
+            double percolationThreshold = opened / gridSize;
+            this.percolationThresholds[i] = percolationThreshold;
+        }
+
+        /** Mean */
+        this.mean = StdStats.mean(this.percolationThresholds);
+
+        /** Standard Deviation */
+        if (this.trials == 1) this.stddev = Double.NaN;
+        else {
+            this.stddev = Math.sqrt(StdStats.stddev(this.percolationThresholds));
+        }
+    }
+
+    private Site findRandomClosedSite(Percolation p) {
+        Integer row = StdRandom.uniform(1, this.n);
+        Integer col = StdRandom.uniform(1, this.n);
+        while (p.isOpen(row, col)) {
+            row = StdRandom.uniform(1, this.n);
+            col = StdRandom.uniform(1, this.n);
+        }
+
+        return new Site(row, col);
     }
 
     /** sample mean of percolation threshold */
     public double mean() {
-        throw new UnsupportedOperationException();
+        return this.mean;
     }
 
     /** sample standard deviation of percolation threshold */
     public double stddev() {
-        throw new UnsupportedOperationException();
+        return this.stddev;
     }
 
     /** low  endpoint of 95% confidence interval */
     public double confidenceLo() {
-        throw new UnsupportedOperationException();
+        return this.mean - ((1.96d * this.stddev) / Math.sqrt(this.trials));
     }
 
     /** high endpoint of 95% confidence interval */
     public double confidenceHi() {
-        throw new UnsupportedOperationException();
+        return this.mean + ((1.96d * this.stddev) / Math.sqrt(this.trials));
     }
 
     /** test client (described below) */
     public static void main(String[] args) {
-        throw new UnsupportedOperationException();
+        if (args.length != 2) throw new IllegalArgumentException("Must provide 2 command line arguments");
+        int n = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+
+        PercolationStats ps = new PercolationStats(n, T);
+
+        double mean = ps.mean();
+        double stddev = ps.stddev();
+        double confidenceLow = ps.confidenceLo();
+        double confidenceHi = ps.confidenceHi();
+
+        StdOut.println("mean = " + mean);
+        StdOut.println("stddev = " + stddev);
+        StdOut.println("95% confidence interval = " + confidenceLow + ", " + confidenceHi);
+    }
+
+    public class Site<Row, Col> {
+        public final Row row;
+        public final Col col;
+
+        public Site(Row row, Col col) {
+            this.row = row;
+            this.col = col;
+        }
     }
 }
+
